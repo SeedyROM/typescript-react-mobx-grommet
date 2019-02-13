c0e79fc0963cb1e66e0210379a3ec93fd58f8897
Zack Kollar <rallokkcaz@gmail.com>
2019-02-13 03:33:39 -0800

    Added gimmicky default app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,32 @@
-import * as React from 'react';
-
 import { Box, Button, Image } from 'grommet';
+import * as React from 'react';
+import styled, { keyframes } from 'styled-components';
 
 import logo from '../assets/logo.svg';
 import './App.css';
 import AppWrapper from './AppWrapper';
 import Search from './Search';
+
+const logoAnimation = keyframes`
+  0% {
+    transform: rotate(0deg) scale(1);
+  }
+
+  50% {
+    transform: rotate(180deg) scale(1.2);
+  }
+
+  100% {
+    transform: rotate(360deg) scale(1);
+  }
+`;
+
+const Logo = styled.img`
+  animation: ${logoAnimation} infinite 4s linear;
+  mix-blend-mode: exclusion;
+  width: 200px;
+  margin-bottom: 1rem;
+`;
 
 const Page = (props: any) => (
   <Box
@@ -19,14 +40,19 @@ const Page = (props: any) => (
   </Box>
 );
 
+const goToGithub = () => {
+  location.href = 'https://github.com/SeedyROM/typescript-react-mobx-grommet';
+};
+
 export default () => (
   <AppWrapper>
     <Page background="neutral-2">
-      <Image height="200px" margin={{bottom: '1rem'}} src={logo} />
+      <Logo src={logo} />
       <Search />
       <Button
         label="Typescript, React, Grommet and MobX"
         primary={true}
+        onClick={goToGithub}
       />
     </Page>
   </AppWrapper>
