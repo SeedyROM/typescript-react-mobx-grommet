57874e822c078b58d5619713e96d76cec7f5a7cc
Zack Kollar <rallokkcaz@gmail.com>
2019-02-13 23:34:32 -0800

    Whatever is here

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
 import { action, observable } from 'mobx';
-import { observer } from 'mobx-react';
 import { SearchBarStore } from './search-bar';
 
 export class ApplicationStore {
--- src/store/search-bar.ts
+++ src/store/search-bar.ts
@@ -1,5 +1,4 @@
 import { action, observable } from 'mobx';
-import { observer } from 'mobx-react';
 
 export class SearchBarStore {
     @observable
